d63206661c9080e956df7cc7780b201c1edafca7
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2021-09-25 19:14:41 +0630

    extracted table body component

--- src/components/MoviesTable.jsx
+++ src/components/MoviesTable.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TableHeader from './common/TableHeader';
+import TableBody from './common/TableBody';
 import Like from './common/Like';
 
 const MoviesTable = ({ movies, sortColumn, onLike, onDelete, onSort }) => {
@@ -8,31 +9,21 @@ const MoviesTable = ({ movies, sortColumn, onLike, onDelete, onSort }) => {
     { label: 'Genre', path: 'genre.name' },
     { label: 'Stock', path: 'numberInStock' },
     { label: 'Rate', path: 'dailyRentalRate' },
-    { key: 'like' },
-    { key: 'delete' },
+    { key: 'like', content: (movie) => <Like liked={movie.liked} onClick={() => onLike(movie)} /> },
+    {
+      key: 'delete',
+      content: (movie) => (
+        <button className="btn btn-danger btn-sm" onClick={() => onDelete(movie)}>
+          Delete
+        </button>
+      ),
+    },
   ];
 
   return (
     <table className="table my-4">
       <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
-      <tbody>
-        {movies.map((movie) => (
-          <tr key={movie._id}>
-            <td>{movie.title}</td>
-            <td>{movie.genre.name}</td>
-            <td>{movie.numberInStock}</td>
-            <td>{movie.dailyRentalRate}</td>
-            <td>
-              <Like liked={movie.liked} onClick={() => onLike(movie)} />
-            </td>
-            <td>
-              <button className="btn btn-danger btn-sm" onClick={() => onDelete(movie)}>
-                Delete
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
+      <TableBody data={movies} columns={columns} />
     </table>
   );
 };
---
+++ src/components/common/TableBody.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import _ from 'lodash';
+
+const TableBody = ({ data, columns }) => {
+  const renderCell = (item, column) => {
+    if (column.content) return column.content(item);
+    return _.get(item, column.path);
+  };
+
+  return (
+    <tbody>
+      {data.map((item) => (
+        <tr key={item._id}>
+          {columns.map((column) => (
+            <td key={column.label || column.key}>{renderCell(item, column)}</td>
+          ))}
+        </tr>
+      ))}
+      <tr>
+        <td></td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default TableBody;
