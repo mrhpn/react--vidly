998b357ded6469f98a7d8d6e17ed441fe15b63aa
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2021-09-26 13:36:30 +0630

    validated with joi

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Joi from 'joi-browser';
+
 import Input from './common/Input';
 
 class Login extends React.Component {
@@ -7,14 +9,19 @@ class Login extends React.Component {
     errors: {},
   };
 
+  schema = {
+    username: Joi.string().required().label('Username'),
+    password: Joi.string().required().label('Password'),
+  };
+
   validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
+
     const errors = {};
-    const { account } = this.state;
-
-    if (account.username.trim() === '') errors.username = 'Username is required';
-    if (account.password.trim() === '') errors.password = 'Password is required';
-
-    return Object.keys(errors).length === 0 ? null : errors;
+    for (let e of error.details) errors[e.path[0]] = e.message;
+    return errors;
   };
 
   handleSubmit = (e) => {
