8d452a518ec9c5b4a49e7496a7bad09886d62729
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2021-10-02 16:10:32 +0630

    refactored movie service

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -8,19 +8,23 @@ export function getMovies() {
 }
 
 export function getMovie(movieId) {
-  return http.get(apiEndpoint + '/' + movieId);
+  return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    return http.put(apiEndpoint + '/' + movie._id, body);
+    return http.put(movieUrl(movie._id), body);
   }
 
   return http.post(apiEndpoint, movie);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(apiEndpoint + '/' + movieId);
+  return http.delete(movieUrl(movieId));
+}
+
+function movieUrl(id) {
+  return `${apiEndpoint}\${id}`;
 }
