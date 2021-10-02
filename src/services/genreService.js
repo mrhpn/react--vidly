ac8158219049396c8a7c92b0fc4f62e8f9a6ec07
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2021-10-03 00:29:37 +0630

    prepared for deployment

--- src/services/genreService.js
+++ src/services/genreService.js
@@ -1,6 +1,5 @@
-import { apiUrl } from '../config.json';
 import http from './httpService';
 
 export function getGenres() {
-  return http.get(apiUrl + '/genres');
+  return http.get('/genres');
 }
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,7 +1,6 @@
-import { apiUrl } from '../config.json';
 import http from './httpService';
 
-const apiEndpoint = apiUrl + '/movies';
+const apiEndpoint = '/movies';
 
 export function getMovies() {
   return http.get(apiEndpoint);
